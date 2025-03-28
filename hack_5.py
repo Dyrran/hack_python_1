8d64ed149d9932586fe7cbbc9af4b525c1861af1
Randy Parra <randyparra@hotmail.com>
2025-03-27 22:34:25 -0400

    Hack 1 al 4 listos

--- hack_5.py
+++ hack_5.py
@@ -4,5 +4,7 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    #...
-    return result  
+    result = result[0] + "00" + result[3] + "1" + result[5] + "@" + result[7]
+    return result 
+
+print(fn_hack_5())
