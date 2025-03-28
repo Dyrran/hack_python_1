06d60f2653d7d1e1303b698785a420b243038319
Dyrran <randyparra@hotmail.com>
2025-03-27 22:56:30 -0400

    Realizado hasta el hack 6

--- hack_6.py
+++ hack_6.py
@@ -3,6 +3,10 @@ loop: for [] output => [0,1,2,3,4,5]
 """
 
 def fn_hack_6():
+    text = "012345"
     result = []
-    #...
-    return result  
+    for char in text:
+        result.append(int(char))
+    return result
+
+print(fn_hack_6())
